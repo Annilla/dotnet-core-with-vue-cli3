b749ccc3e3bb7a86bc48df59340679f62d96348f
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2019-10-01 17:25:38 +0800

    Package json add run hmr script

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   outputDir: 'wwwroot',
-  baseUrl: "/",
+  publicPath: "/",
   chainWebpack: config => {
-      // aspnet uses the other hmr so remove this one
-      config.plugins.delete('hmr');
+    // https://github.com/vuejs/vue-cli/issues/3603#issuecomment-483913563
+    // remove vue-cli-service's progress output
+    config.plugins.delete('progress')
+    // https://intellitect.com/quickly-configure-asp-net-core-api-to-work-with-vue-cli-3/
+    // aspnet uses the other hmr so remove this one
+    config.plugins.delete('hmr');
   }
 }
